30523159a81ce2be856a2391f1044dce583ef579
James Cori <jamescori@gmail.com>
2020-05-26 20:22:29 -0400

    Removing the dynamo check from health check so it stops rebooting

--- src/services/HealthCheckService.js
+++ src/services/HealthCheckService.js
@@ -1,28 +1,28 @@
 /**
  * Health Check Service
  */
-const config = require('config')
-const errors = require('../common/errors')
-const { DynamoDB } = require('../models')
+// const config = require('config')
+// const errors = require('../common/errors')
+// const { DynamoDB } = require('../models')
 
 /**
  * Check if the DynamoDB connection is active
  */
 async function check () {
-  const checkDynamoDB = new Promise((resolve, reject) => {
-    DynamoDB.listTables({}, (err, data) => {
-      if (err) {
-        return reject(new errors.ServiceUnavailable('DynamoDB instance cannot be reached'))
-      }
-      return resolve()
-    })
-  })
+  // const checkDynamoDB = new Promise((resolve, reject) => {
+  //   DynamoDB.listTables({}, (err, data) => {
+  //     if (err) {
+  //       return reject(new errors.ServiceUnavailable('DynamoDB instance cannot be reached'))
+  //     }
+  //     return resolve()
+  //   })
+  // })
 
-  const timeOutBreak = new Promise((resolve, reject) => {
-    setTimeout(reject, config.DYNAMODB.TIMEOUT, new errors.ServiceUnavailable('DynamoDB instance cannot be reached'))
-  })
+  // const timeOutBreak = new Promise((resolve, reject) => {
+  //   setTimeout(reject, config.DYNAMODB.TIMEOUT, new errors.ServiceUnavailable('DynamoDB instance cannot be reached'))
+  // })
 
-  await Promise.race([checkDynamoDB, timeOutBreak])
+  // await Promise.race([checkDynamoDB, timeOutBreak])
 
   return {
     checksRun: 1
