c74537fbbef74599fab124a0da899ab53d0e70d9
James Cori <jamescori@gmail.com>
2020-06-08 21:06:09 -0400

    Removing the debug logging

--- app-routes.js
+++ app-routes.js
@@ -39,7 +39,7 @@ module.exports = (app) => {
 
         actions.push((req, res, next) => {
           if (req.authUser.isMachine) {
-            logger.warn(`Request Auth User ${req.authUser} calling ${controllerPath} ${method}`)
+            // logger.warn(`Request Auth User ${req.authUser} calling ${controllerPath} ${method}`)
             // M2M
             if (!req.authUser.scopes || !helper.checkIfExists(def.scopes, req.authUser.scopes)) {
               next(new errors.ForbiddenError('You are not allowed to perform this action!'))
