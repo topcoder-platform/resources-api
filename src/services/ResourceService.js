38a8b7771be9acdc6fa589a44e4261252b3e909f
James Cori <jamescori@gmail.com>
2020-08-24 17:21:08 -0400

    Updating the getResources to be better at catching legacy IDs and not flooding the logs with output

--- src/services/ResourceService.js
+++ src/services/ResourceService.js
@@ -6,7 +6,7 @@ const _ = require('lodash')
 const config = require('config')
 const Joi = require('joi')
 const uuid = require('uuid/v4')
-// const { validate: uuidValidate } = require('uuid')
+const uuidV4 = require('uuidv4')
 const moment = require('moment')
 const helper = require('../common/helper')
 const logger = require('../common/logger')
@@ -45,11 +45,15 @@ async function checkAccess (currentUser, resources) {
  * @returns {Array} the search result
  */
 async function getResources (currentUser, challengeId, roleId, page, perPage) {
-  // if (!uuidValidate(challengeId)) {
-  //   throw new errors.BadRequestError(`Challenge ID ${challengeId} must be a valid UUID`)
-  // }
-  // Verify that the challenge exists
-  await helper.getRequest(`${config.CHALLENGE_API_URL}/${challengeId}`)
+  if (!uuidV4.isUuid(challengeId)) {
+    throw new errors.BadRequestError(`Challenge ID ${challengeId} must be a valid v5 Challenge Id (UUID)`)
+  }
+  try {
+    // Verify that the challenge exists
+    await helper.getRequest(`${config.CHALLENGE_API_URL}/${challengeId}`)
+  } catch (e) {
+    throw new errors.NotFoundError(`Challenge ID ${challengeId} not found`)
+  }
 
   const boolQuery = []
   const mustQuery = []
