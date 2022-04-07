8c3ddc890a7f37aeda2637c68df4e24937d69c75
Thomas Kranitsas <thomas.kranitsas@topcoder.com>
2022-04-07 17:48:39 +0300

    clean up

--- app-routes.js
+++ app-routes.js
@@ -57,7 +57,7 @@ module.exports = (app) => {
                 }
                 next()
               } catch (e) {
-                throw new errors.ForbiddenError('failed to check')
+                next()
               }
             }
           })
