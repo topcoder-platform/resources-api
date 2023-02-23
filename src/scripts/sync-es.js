dde5e751e6fd8ec90f4fd3c7a0bd6fa8619f6f22
Rakib Ansary <rakibansary@gmail.com>
2023-02-23 17:30:58 +0600

    feat: script to index resource and resource_role
    Signed-off-by: Rakib Ansary <rakibansary@gmail.com>

---
+++ src/scripts/sync-es.js
@@ -0,0 +1,72 @@
+/**
+ * Migrate Data from Dynamo DB to ES
+ */
+
+const config = require('config')
+const logger = require('../common/logger')
+const helper = require('../common/helper')
+const models = require('../models')
+const _ = require('lodash')
+
+const esClient = helper.getESClient()
+
+async function indexResource (indexName, resource) {
+  if (indexName == null) {
+    console.log('Invalid index name')
+    return
+  }
+  console.log('Indexing resource', resource.id)
+  try {
+    await esClient.update({
+      index: indexName,
+      id: resource.id,
+      body: { doc: resource, doc_as_upsert: true }
+    })
+  } catch (err) {
+    console.log('Error indexing resource', resource.id, err)
+  }
+}
+
+function getIndex (table) {
+  switch (table) {
+    case 'Resource':
+      return config.get('ES.ES_INDEX')
+    case 'ResourceRole':
+      return 'resource_roles'
+  }
+}
+
+/*
+ * Migrate records from DB to ES
+ */
+async function migrateRecords () {
+  const tablesToIndex = ['Resource']
+  for (const table of tablesToIndex) {
+    console.log('Indexing table', table)
+    let results = await models['Resource'].scan().exec()
+    let lastKey = results.lastKey
+
+    for (const resource of results) {
+      await indexResource(resource)
+    }
+
+    while (!_.isUndefined(results.lastKey)) {
+      const results = await models['Resource'].scan().startAt(lastKey).exec()
+      for (const resource of results) {
+        await indexResource(getIndex(table), resource)
+      }
+
+      lastKey = results.lastKey
+    }
+  }
+}
+
+migrateRecords()
+  .then(() => {
+    logger.info('Done')
+    process.exit()
+  })
+  .catch((err) => {
+    logger.logFullError(err)
+    process.exit(1)
+  })
